cfba5817e5dabd440c59a9897ea714feb1251b4c
davo95076 <davo.aleqsandryan@mail.ru>
2024-05-15 18:48:27 +0400

    slider

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const img: string[] = [
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+  ];
+
+  return (
+    <div className="App">
+      <h2>Slider: </h2>
+    </div>
+  );
+}
+
+export default App;
